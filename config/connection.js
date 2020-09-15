2aeb4b14fb1f5bc2b400a5e1dfea0aba21a3be74
Scott <scottdouglas238@gmail.com>
2020-09-14 21:22:35 -0700

    Ready now

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    port: 3306,
-    user: "rb52db47bfdceb1",
-    password: "422c01fb",
-    database: "heroku_af2b417cdb51a32.burgers"
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'us8oqd5alz7497lt',
+    password: 'xz412qg9p04cxnek',
+    database: 'zseiuh9qy21wcf1p'
   });
+};                                                                                                   
+
+// const connection = mysql.createConnection({
+//     host: "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "us8oqd5alz7497lt",
+//     password: "xz412qg9p04cxznek",
+//     database: "zseiuh9qy21wcf1p"
+//   });
 
   connection.connect(function(err) {
     if (err) throw err;
